58d0b484a59562e79809e1a12b6f6979cf650324
RestiAdelia <adeliaresti101@gmail.com>
2025-06-27 15:13:19 +0700

    Create Middleware RoleAdmin

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,14 +4,11 @@
 
 @section('content')
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
-
     <form method="POST" action="{{ route('login') }}" class="login-form">
         @csrf
         <div class="d-flex justify-content-center align-items-center">
             <h2 class="text-center">Login</h2>
         </div>
-
-
         <div>
             <input id="email" type="email" name="email" placeholder="Email" value="{{ old('email') }}" required
                 autofocus>
@@ -19,21 +16,12 @@
                 <div class="error-text">{{ $message }}</div>
             @enderror
         </div>
-
         <div>
             <input id="password" type="password" name="password" placeholder="Password" required>
             @error('password')
                 <div class="error-text">{{ $message }}</div>
             @enderror
         </div>
-
-        <div>
-            <label for="remember">
-                <input id="remember" type="checkbox" name="remember">
-                Remember me
-            </label>
-        </div>
-
         <div>
             <button type="submit">Login</button>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\ResponController;
 
 use App\Http\Controllers\ComplaintController;
 use App\Http\Controllers\JumlahPengaduanController;
+use App\Http\Middleware\RoleAdmin;
 use App\Models\complain;
 
 Route::get('/', [JumlahPengaduanController::class, 'index']);
@@ -18,7 +19,7 @@ Route::get('login', [AuthController::class, 'loginform'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth',RoleAdmin::class])->group(function () {
     Route::get('/homeadmin', [AuthController::class, 'index'])->name('homeadmin');
     Route::get('/complaints/list', [ComplaintController::class, 'list'])->name('complaints.list');
     Route::get('/complain/{id}/respon', [ComplaintController::class, 'formRespon'])->name('complain.formRespon');
@@ -32,10 +33,11 @@ Route::middleware(['auth'])->group(function () {
     Route::resource('respon', ResponController::class);
     Route::resource('category', CategoryController::class)->names('category');
 });
- Route::resource('pelapor', PelaporController::class);
+
+Route::resource('pelapor', PelaporController::class);
 Route::get('/complain', [ComplaintController::class, 'index'])->name('complain.index');
 Route::get('/pelapor/create', [PelaporController::class, 'create'])->name('pelapor.create');
 Route::get('/complain/create/{id_pelapor}', [ComplaintController::class, 'create'])->name('complain.create');
 Route::post('/complain', [ComplaintController::class, 'store'])->name('complain.store');
 Route::post('/complain/store', [ComplaintController::class, 'store'])->name('complain.store');
-Route::get('/complain/{id}', [ComplaintController::class, 'show'])->name('complain.show'); // Untuk 1 data spesifik
+Route::get('/complain/{id}', [ComplaintController::class, 'show'])->name('complain.show');
